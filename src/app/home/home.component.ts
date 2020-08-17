f40dec0e4f41c3ac14ee8cff8d401649409276e9
Jessie <jasuncion@willdan.com>
2020-08-17 10:36:51 -0700

    Added token parse from url and service call to validate token

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpService } from '../http.service';
 
 
 @Component({
@@ -12,7 +13,7 @@ export class HomeComponent implements OnInit {
   clickCounter: number = 0;
   name: string = '';
   
-  constructor(private router: Router) { }
+  constructor(private router: Router, private _http: HttpService) { }
 
   ngOnInit(): void {
     var match = (window.location.hash) ? window.location.hash.match(/#access_token=([^&]+)/) : false;
@@ -20,6 +21,12 @@ export class HomeComponent implements OnInit {
     console.log("match[1] = " + match[1]);
     if (match[1]) {
       console.log("Found token");
+      sessionStorage.setItem('token', match[1]);
+
+      this._http.doAuth(match[1]).subscribe(data => {
+        console.log(data);
+      });
+
       this.router.navigate(['/list']);
     } else {
       window.location.href = "http://localhost:4200/";
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +10,34 @@ export class HttpService {
 
   getBeer (){
     return this._http.get('https://api.openbrewerydb.org/breweries')
+  }
+
+  doAuth (token){
+    //console.log('https://localhost:44328/api/Status', { headers: this.getAPIHeaders(token) });
+    //return this._http.get('https://localhost:44328/api/Status', { headers: this.getAPIHeaders(token) })
+
+    const httpHeaders: HttpHeaders = new HttpHeaders({
+      'Authorization': 'Bearer ' + token,
+      'Access-Control-Allow-Origin':'*'
+    });
+    // const headers = new HttpHeaders({
+    //   Content-Type: 'application/json'
+    // .set('Accept', 'application/json')
+    // .set('Access-Control-Allow-Headers', 'Content-Type')
+
+    // });
+    //let params = new URLSearchParams();
+    //params.append("Authorization", 'Bearer ' + token)
+    
+    
+
+    return this._http.get('https://localhost:44328/api/Status', { headers: httpHeaders })
+  }
+
+  getAPIHeaders(token) {
+    let headers = {
+        Authorization: 'Bearer ' + token
+    };
+    return headers;
   }
 }
